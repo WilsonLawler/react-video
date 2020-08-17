a2926b7a3606137d9c39a17128348ed7bb12c06a
roger.huang <roger.huang@udngroup.com>
2020-08-17 23:00:11 +0800

    Updates

---
+++ precache-manifest.2c280477500d7d13eb55260d156c59a8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29895fd958a547d4d8c08ca63f1da076",
+    "url": "/react-video/index.html"
+  },
+  {
+    "revision": "1e6e9ec350eeb1ff2e09",
+    "url": "/react-video/static/css/2.bf7760c3.chunk.css"
+  },
+  {
+    "revision": "1e6e9ec350eeb1ff2e09",
+    "url": "/react-video/static/js/2.b5e51d06.chunk.js"
+  },
+  {
+    "revision": "03e1b58e8b47d0571d9669f8f9a6bb30",
+    "url": "/react-video/static/js/2.b5e51d06.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c83f365f4de859beda3",
+    "url": "/react-video/static/js/main.2da6b62c.chunk.js"
+  },
+  {
+    "revision": "33aebb00ae85e0d0b1c6",
+    "url": "/react-video/static/js/runtime-main.184e31df.js"
+  },
+  {
+    "revision": "97bc08a7cf8f9942f00d9fb034437f7e",
+    "url": "/react-video/static/media/videoShort.97bc08a7.mp4"
+  }
+]);
