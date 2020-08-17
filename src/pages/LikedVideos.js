d69c6311f4f36b6087aaab35025b59e9d5d939fe
roger.huang <roger.huang@udngroup.com>
2020-08-18 07:45:33 +0800

    update file

--- src/pages/LikedVideos.js
+++ src/pages/LikedVideos.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { connect } from "react-redux";
+import Button from '../styles/Button';
 import { StyledTrending } from "./Trending";
 import TrendingCard from "../components/TrendingCard";
 import { getLikedVideos, getVideo } from "../actions";
@@ -13,9 +14,14 @@ const LikedVideos = ({ isFetching, videos, getLikedVideos }) => {
       <h2>我的收藏</h2>
 
       {videos?.length === 0 && (
-        <p className="secondary">
-          您收藏的影片將會顯示於此
+        <>
+          <p className="secondary">
+            您收藏的影片將會顯示於此
         </p>
+          <Link to='/1'>
+            <Button>回首頁</Button>
+          </Link>
+        </>
       )}
 
       {videos.map((video) => (
